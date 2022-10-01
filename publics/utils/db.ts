6f40b09004362f389975f0f3977e6aae098289c2
AG <agentaidan@gmail.com>
2022-10-01 15:16:24 -0500

    complete quickstart

--- publics/utils/db.ts
+++ publics/utils/db.ts
@@ -3,4 +3,8 @@ import { createClient } from '@supabase/supabase-js'
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
 
+<<<<<<< HEAD
 export const supabase = createClient(supabaseUrl!, supabaseAnonKey!)
+=======
+export const supabase = createClient(supabaseUrl, supabaseAnonKey)
+>>>>>>> d3ddfc8 (complete quickstart)
