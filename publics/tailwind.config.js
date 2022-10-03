0ac47f860a8c389b439e31f1fadfd8f474e6ce8c
AG <agentaidan@gmail.com>
2022-10-03 15:15:40 -0500

    add npm and fix tailwind

--- publics/tailwind.config.js
+++ publics/tailwind.config.js
@@ -1,8 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./**/*.{html,js}"],
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {},
   },
-  plugins: [require("daisyui")],
+  plugins: [require('daisyui')],
 }
