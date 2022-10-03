86fe09e5d8a9e82c8898e337509e7dbcea0195c7
AG <agentaidan@gmail.com>
2022-10-03 14:12:04 -0500

    try to get google working

--- publics/components/Auth.tsx
+++ publics/components/Auth.tsx
@@ -6,44 +6,46 @@ export default function Auth() {
   const [email, setEmail] = useState('')
 
   const handleLogin = async () => {
-    const { user, session, error } = await supabase.auth.signIn({
-      // provider can be 'github', 'google', 'gitlab', and more
-      provider: 'github'
-    }, 
-    // {
-    //   redirectTo: 'https://example.com/welcome'
-    // }
+    const res = await supabase.auth.signInWithOAuth({
+      provider: 'google'
+    },
+      // {
+      //   redirectTo: 'https://example.com/welcome'
+      // }
     )
+    console.log(res)
   }
 
   return (
     <div className="row flex-center flex">
       <div className="col-6 form-widget">
         <h1 className="header">Supabase + Next.js</h1>
-        <p className="description">
-          Sign in with your Rice Gmail
-        </p>
-        <div>
-          <input
-            className="inputField"
-            type="email"
-            placeholder="Your email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-        </div>
-        <div>
-          <button
-            onClick={(e) => {
-              e.preventDefault()
-              handleLogin()
-            }}
-            className="button block"
-            disabled={loading}
-          >
-            <span>Sign In With Rice Google</span>
-          </button>
-        </div>
+        <form>
+          <p className="description">
+            Sign in with your Rice Gmail
+          </p>
+          <div>
+            <input
+              className="inputField"
+              type="email"
+              placeholder="Your email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+          <div>
+            <button
+              onClick={(e) => {
+                e.preventDefault()
+                handleLogin()
+              }}
+              className="button block"
+              disabled={loading}
+            >
+              <span>Sign In With Rice Google</span>
+            </button>
+          </div>
+        </form>
       </div>
     </div>
   )
