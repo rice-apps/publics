0a9cdb4b98a7b7c40ea43399d21532ac52c1bf5a
spoken-angus <angusjelinek@gmail.com>
2022-10-10 09:13:33 -0400

    type fixes

--- publics/utils/db.ts
+++ publics/utils/db.ts
@@ -3,8 +3,4 @@ import { createClient } from '@supabase/supabase-js'
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
 
-<<<<<<< HEAD
 export const supabase = createClient(supabaseUrl!, supabaseAnonKey!)
-=======
-export const supabase = createClient(supabaseUrl, supabaseAnonKey)
->>>>>>> d3ddfc8 (complete quickstart)
