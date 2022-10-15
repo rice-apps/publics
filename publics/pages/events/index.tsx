c18ccfd7880230e85c559b6ce3ff9559fbe894b5
gueyman <63422466+gueyman@users.noreply.github.com>
2022-10-15 15:30:54 -0500

    Added Grid and EventCard component

--- publics/pages/events/index.tsx
+++ publics/pages/events/index.tsx
@@ -15,7 +15,6 @@ export default function Events(props: any) {
     return (
         <div className="grid grid-cols-4 gap-4">
             {mockData.map((event) => <EventCard event={event}/>)}
-            <div>01</div>
             
         </div>
     )
