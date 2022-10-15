6d29ab37c1d5abd2c1e1f278fe214200fceee36a
ey91 <ey9@rice.edu>
2022-10-15 15:04:58 -0500

    WIP Events Grid

---
+++ publics/components/EventCard.tsx
@@ -0,0 +1,15 @@
+export default function EventCard(props: any) {
+   
+    return (
+        <div className="card w-96 bg-base-100 shadow-xl">
+            <figure><img src="https://placeimg.com/400/225/arch" alt="Shoes" /></figure><div className="card-body">
+                <h2 className="card-title">{props.event.name}</h2>
+                <p>{props.event.description}?</p>
+                <div className="card-actions justify-end">
+                    <button className="btn btn-primary">More Information</button>
+                </div>
+            </div>
+        </div>
+    )
+}
+
---
+++ publics/pages/events/index.tsx
@@ -0,0 +1,22 @@
+import EventCard from "../../components/EventCard"
+const mockData = [{
+    name : "NOD",
+    description: "Horny",
+    date: new Date(2022,10,15, 22,0)
+},
+{
+    name: "Baker 13",
+    description: "Naked run",
+    date: new Date(2022, 10, 13, 23, 0)
+}
+]
+
+export default function Events(props: any) {
+    return (
+        <div className="grid grid-cols-4 gap-4">
+            {mockData.map((event) => <EventCard event={event}/>)}
+            <div>01</div>
+            
+        </div>
+    )
+}
