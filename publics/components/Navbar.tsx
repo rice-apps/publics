4b5e76a87438636d952e4b916bbeac991a648081
AG <agentaidan@gmail.com>
2022-10-15 14:23:50 -0500

    responsive navbar

--- publics/components/Navbar.tsx
+++ publics/components/Navbar.tsx
@@ -1,10 +1,29 @@
 export default function Navbar() {
     return (
         <div className="navbar bg-base-100">
-            <div className="flex-1">
+            <div className="navbar-start">
+                <div className="dropdown">
+                    <label tabIndex={0} className="btn btn-ghost lg:hidden">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+                    </label>
+                    <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+                        <li><a>Item 1</a></li>
+                        <li tabIndex={0}>
+                            <a className="justify-between">
+                                Parent
+                                <svg className="fill-current" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M8.59,16.58L13.17,12L8.59,7.41L10,6L16,12L10,18L8.59,16.58Z" /></svg>
+                            </a>
+                            <ul className="p-2">
+                                <li><a>Submenu 1</a></li>
+                                <li><a>Submenu 2</a></li>
+                            </ul>
+                        </li>
+                        <li><a>Item 3</a></li>
+                    </ul>
+                </div>
                 <a className="btn btn-ghost normal-case text-xl">daisyUI</a>
             </div>
-            <div className="flex-none">
+            <div className="navbar-center hidden lg:flex">
                 <ul className="menu menu-horizontal p-0">
                     <li><a>Item 1</a></li>
                     <li tabIndex={0}>
@@ -12,13 +31,16 @@ export default function Navbar() {
                             Parent
                             <svg className="fill-current" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z" /></svg>
                         </a>
-                        <ul className="p-2 bg-base-100">
+                        <ul className="p-2">
                             <li><a>Submenu 1</a></li>
                             <li><a>Submenu 2</a></li>
                         </ul>
                     </li>
                     <li><a>Item 3</a></li>
                 </ul>
+            </div>
+            <div className="navbar-end">
+                <a className="btn">Get started</a>
             </div>
         </div>
     )
