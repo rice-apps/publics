48ba1e232e3c5baa5a0c2e008848b28d0a1bede6
Evan-Stegall <ers11@rice.edu>
2022-10-19 13:24:32 -0500

    made event code cleaner and add ContextProvider to app file

--- publics/pages/_app.tsx
+++ publics/pages/_app.tsx
@@ -1,13 +1,29 @@
 import '../styles/global.css'
+import React, {useState} from 'react'
 import type { AppProps } from 'next/app'
-import Layout from '../components/Layout'
+import { useRouter } from 'next/router'
+import { createBrowserSupabaseClient } from '@supabase/auth-helpers-nextjs'
+import { SessionContextProvider, Session } from '@supabase/auth-helpers-react'
 
-function MyApp({ Component, pageProps }: AppProps) {
+
+function MyApp({ 
+  Component, 
+  pageProps 
+}: AppProps<{
+  initialSession : Session
+}>) {
+  const router = useRouter()
+  const [supabaseClient] = useState(() => createBrowserSupabaseClient())
+
   return (
-    <Layout>
+    <SessionContextProvider
+    supabaseClient={supabaseClient}
+    initialSession={pageProps.initialSession}
+    >
       <Component {...pageProps} />
-    </Layout>
+    </SessionContextProvider>
   )
+  //You can now determine if a user is authenticated by checking that the user object returned by the useUser() hook is defined.
 }
 
 export default MyApp
