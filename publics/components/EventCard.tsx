aca04ff69b3f14a0e7860ec329e58ddc611cc3d3
AG <agentaidan@gmail.com>
2022-10-22 15:24:01 -0500

    add types, add card hover, add redirect

--- publics/components/EventCard.tsx
+++ publics/components/EventCard.tsx
@@ -1,12 +1,20 @@
-export default function EventCard(props: any) {
+import type { ListEvent } from "../utils/types"
+import { useRouter } from 'next/router'
+
+type Props = {
+    event: ListEvent
+}
+
+export default function EventCard(props: Props) {
+    const router = useRouter()
     console.log("props" + props)
     return (
-        <div className="card w-full bg-base-100 shadow-xl">
+        <div className="card w-full bg-base-100 shadow-xl hover:scale-110 transition-transform">
             <figure><img src="https://placeimg.com/400/225/arch" alt="Shoes" /></figure><div className="card-body">
                 <h2 className="card-title">{props.event.name}</h2>
                 <p>{props.event.description}</p>
                 <div className="card-actions justify-end">
-                    <button className="btn btn-primary">More Information</button>
+                    <button className="btn btn-primary" onClick={() => router.push(`events/${props.event.slug}`)}>More Information</button>
                 </div>
             </div>
         </div>
