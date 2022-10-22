9edd78f9c12e3f358bda9f4128fd73d787a22d7c
bh39 <bh39@rice.edu>
2022-10-22 14:17:56 -0500

    connect to backend

--- publics/pages/events/[slug].tsx
+++ publics/pages/events/[slug].tsx
@@ -1,26 +1,29 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
 import { supabase } from '../../utils/db'
-
-import { useRouter } from 'next/router'
 
 export async function getServerSideProps(context) {
     console.log(context)
 
-    const { data, error } = await supabase.from("events").select(`name, description, event_datetime, registration_datetime, registration`).eq("slug", context.params.slug)
+    const { data, error } = await supabase.from("events").select(`name, description, event_datetime, registration_datetime, registration, capacity, waitlist_size`).eq("slug", context.params.slug)
 
     if (error) throw (error)
-    console.log(data)
+
+    // if no event is found, redirect to 404 page
+    if (data.length === 0) {
+        return {
+            notFound: true
+        }
+    }
     return {
         props: { data },
     }
 }
 const details = (props) => {
+    // const router = useRouter()
+    // const { eventName } = router.query
 
-    const event = props.data
-
-    const router = useRouter()
-    const { eventName } = router.query
+    const event = props.data[0]
+    event.registration_datetime = new Date(event.registration_datetime)
+    event.event_datetime = new Date(event.event_datetime)
 
     return (
         <div>
@@ -28,7 +31,7 @@ const details = (props) => {
                 <div className="hero min-h-screen bg-base-200">
                     <div className="hero-content">
                         <div className="max-w-md">
-                            <h1 className="text-5xl font-bold">{eventName} Details!</h1>
+                            <h1 className="text-5xl font-bold">{event.name} Details!</h1>
 
                             <p>Name: {event.name}</p>
                             <p>Event Date: {event.event_datetime.getMonth() + 1 + "/" + event.event_datetime.getDate()}</p>
