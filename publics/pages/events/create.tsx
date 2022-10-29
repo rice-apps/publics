9c56a4b7b324cc8e3e4e3c97fb6fb6e33ad5ad96
AG <agentaidan@gmail.com>
2022-10-29 15:20:07 -0500

    do design
    Co-authored-by: Shivam Pathak <pathaks1@users.noreply.github.com>
    Co-authored-by: nram1 <nram1@users.noreply.github.com>

--- publics/pages/events/create.tsx
+++ publics/pages/events/create.tsx
@@ -3,53 +3,78 @@ import { useState, useEffect } from 'react'
 import { supabase } from '../../utils/db'
 import { useRouter } from 'next/router'
 
-
 export default function Create() {
   const router = useRouter()
-  
+
   const [name, setName] = useState(String)
-  const [capacity, setCapacity] = useState(Number)
-  const [signup, setSignup] = useState(Number)
-  const [waitlist, setWaitlist] = useState(Number)
-  const [description, setDescription] = useState(String)
-  const [eventDateTime, setEventDateTime] = useState(Date)
-  const [signupDateTime, setSignupDateTime] = useState(Date)
   const [slug, setSlug] = useState(String)
+  const [eventDateTime, setEventDateTime] = useState(Date)
+  const [host, setHost] = useState(String)
+  const [location, setLocation] = useState(String)
+  const [capacity, setCapacity] = useState(Number)
+  const [description, setDescription] = useState(String)
+
+  const [registration, setRegistration] = useState(Boolean)
+  const [registrationDatetime, setRegistrationDatetime] = useState(Date)
+  const [signupSize, setSignupSize] = useState(Number)
+  const [waitlistSize, setWaitlistSize] = useState(Number)
+
+  const [orgs, setOrgs] = useState([])
 
   /*
   function refreshPage() {
     window.location.reload();
   }
   */
-  
-  function printing() {
-    console.log(name);
-    console.log(capacity);
-    console.log(signup);
-    console.log(waitlist);
-    console.log(description);
-    console.log(eventDateTime);
-    console.log(signupDateTime);
-    console.log(slug);
+
+  async function getOrgs() {
+    const { data: { user } } = await supabase.auth.getUser()
+    console.log(user)
+    const { data: orgs, error } = await supabase
+      .from('organizations_admins')
+      .select('organization ( id, name )')
+      .eq('profile', user?.id)
+    console.log(orgs)
+    if (error) {
+      throw error
+    }
+    if (orgs) {
+      setOrgs(orgs)
+    }
   }
 
-  
+  useEffect(() => {
+    getOrgs()
+  }, [])
+
+
   async function insert() {
     //setCurrDateTime(Date().toLocaleUpperCase());
     const { error } = await supabase
       .from('events')
-      .insert({name: name, capacity: capacity, signup_size: signup, waitlist_size: waitlist, description: description, 
-               event_datetime: eventDateTime, registration_datetime: signupDateTime, slug: slug})
+      .insert({
+        name: name,
+        slug: slug,
+        event_datetime: eventDateTime,
+        organization: host,
+        location: location,
+        capacity: capacity,
+        description: description,
+        registration: registration,
+        registration_datetime: registrationDatetime,
+        signup_size: signupSize,
+        waitlist_size: waitlistSize
+      })
     if (error) {
       alert(error.message);
     } else {
       router.push(slug);
     }
   }
-  
-  
+
+
   return (
-    <div id = "form">
+    <div id="form">
       <Head>
         <title>Create Event Form</title>
         <meta name="eventcreate" content="Form for creating new event" />
@@ -58,90 +83,107 @@ export default function Create() {
 
       <main>
         <h1>
-          Create an Event with this Form
+          Create an Event
         </h1>
-        <div className="form-control w-full max-w-xs">
-          <label className="label">
-            <span className="label-text">What is the name of your event?</span>
-          </label>
-          <input type="text" placeholder="Type here" className="input input-bordered w-full max-w-xs" />
-        </div>
-        <div className="form-control w-full max-w-xs">
-          <label className="label">
-            <span className="label-text">What is the maximum capacity of your event?</span>
-          </label>
-          <input type="number" placeholder="Type here" className="input input-bordered w-full max-w-xs" />
-        </div>
-        <div className="form-control w-full max-w-xs">
-          <label className="label">
-            <span className="label-text">What is the maximum signup size for your event?</span>
-          </label>
-          <input type="number" placeholder="Type here" className="input input-bordered w-full max-w-xs" />
-        </div>
-        <div className="form-control w-full max-w-xs">
-          <label className="label">
-            <span className="label-text">What is the maximum waitlist size for your event?</span>
-          </label>
-          <input type="number" placeholder="Type here" className="input input-bordered w-full max-w-xs" />
-        </div>
-        <div className="form-control">
-          <label className="label">
-            <span className="label-text">Enter a description for your event here</span>
-          </label> 
-          <textarea className="textarea textarea-bordered max-w-xs h-24" placeholder="Bio"></textarea>
-        </div>
-        <div className="form-control w-full max-w-xs">
-          <label className="label">
-            <span className="label-text">What is the date and time of your event?</span>
-          </label>
-          <input value={eventDate} onChange={(e)=>setEventDate(e.target.value)} type="date" placeholder="Enter valid date" className="input input-bordered w-full max-w-xs" />
-        </div>
-        <div className="form-control w-full max-w-xs">
-          <label className="label">
-            <span className="label-text">What is the starting date and time of registration?</span>
-          </label>
-          <input value={eventTimeStart} onChange={(e)=>setEventTimeStart(e.target.value)} type="time" placeholder="Enter valid time" className="input input-bordered w-full max-w-xs" />
-        </div>
-        <div className="form-control w-full max-w-xs">
-          <label className="label">
-            <span className="label-text">What is the ending time of your event?</span>
-          </label>
-          <input value={eventTimeEnd} onChange={(e)=>setEventTimeEnd(e.target.value)} type="time" placeholder="Enter valid time" className="input input-bordered w-full max-w-xs" />
-        </div>
-        <div className="form-control w-full max-w-xs">
-          <label className="label">
-            <span className="label-text">What is the starting date of registration?</span>
-          </label>
-          <input value={signupDateStart} onChange={(e)=>setSignupDateStart(e.target.value)} type="date" placeholder="Enter valid date" className="input input-bordered w-full max-w-xs" />
-        </div>
-        <div className="form-control w-full max-w-xs">
-          <label className="label">
-            <span className="label-text">What is the starting time of registration?</span>
-          </label>
-          <input value={signupTimeStart} onChange={(e)=>setSignupTimeStart(e.target.value)} type="time" placeholder="Enter valid time" className="input input-bordered w-full max-w-xs" />
-        </div>
-        <div className="form-control w-full max-w-xs">
-          <label className="label">
-            <span className="label-text">What is the ending date of registration?</span>
-          </label>
-          <input value={signupDateEnd} onChange={(e)=>setSignupDateEnd(e.target.value)} type="date" placeholder="Enter valid date" className="input input-bordered w-full max-w-xs" />
-        </div>
-        <div className="form-control w-full max-w-xs">
-          <label className="label">
-            <span className="label-text">What is the ending time of registration?</span>
-          </label>
-          <input value={signupTimeEnd} onChange={(e)=>setSignupTimeEnd(e.target.value)} type="time" placeholder="Enter valid time" className="input input-bordered w-full max-w-xs" />
-        </div>
-        <div className="form-control w-full max-w-xs">
-          <label className="label">
-            <span className="label-text">Enter a unique URL name you want for your event (e.g., nod22)</span>
-          </label>
-          <input type="text" placeholder="Type here" className="input input-bordered w-full max-w-xs" />
-        </div>
         <div>
-          <input type="submit" value="Submit" className="btn" onClick={insert}/>
+          <h2>Event Details</h2>
+          <hr></hr>
         </div>
-        
+        <form>
+          <div className="p-2">
+            <div className="sm:flex">
+              <div className="form-control w-full max-w-xs mr-2">
+                <input value={name} onChange={(e) => setName(e.target.value)} required type="text" className="input input-bordered w-full max-w-xs" />
+                <label className="label">
+                  <span className="label-text-alt">Name of event</span>
+                </label>
+              </div>
+              <div className="form-control w-full max-w-xs mr-2">
+                <input value={slug} onChange={(e) => setSlug(e.target.value)} required type="text" className="input input-bordered w-full max-w-xs" />
+                <label className="label">
+                  <span className="label-text-alt">Slug</span>
+                </label>
+              </div>
+              <div className="form-control w-full max-w-xs">
+                <input value={eventDateTime} onChange={(e) => setEventDateTime(e.target.value)} required type="datetime-local" className="input input-bordered w-full max-w-xs" />
+                <label className="label">
+                  <span className="label-text-alt">Date</span>
+                </label>
+              </div>
+            </div>
+            <div className="sm:flex">
+              <div className="form-control w-full max-w-xs mr-2">
+                <select className="select select-bordered w-full max-w-xs" onChange={(e) => setHost(e.target.value)}>
+                  // add options for orgs
+                  {orgs.length > 0 ? orgs.map((org) => (
+                    <option value={org.organization.id}>{org.organization.name}</option>
+                  )) : <option disabled value="null">You are not a part of any organizations</option>}
+                </select>
+                <label className="label">
+                  <span className="label-text-alt">Host</span>
+                </label>
+              </div>
+              <div className="form-control w-full max-w-xs mr-2">
+                <input value={location} onChange={(e) => setLocation(e.target.value)} required type="text" className="input input-bordered w-full max-w-xs" />
+                <label className="label">
+                  <span className="label-text-alt">Location</span>
+                </label>
+              </div>
+              <div className="form-control w-full max-w-xs">
+                <input value={capacity} onChange={(e) => setCapacity(e.target.valueAsNumber)} required type="text" className="input input-bordered w-full max-w-xs" />
+                <label className="label">
+                  <span className="label-text-alt">Capacity</span>
+                </label>
+              </div>
+            </div>
+            <div className="sm:flex">
+              <div className="form-control w-full max-w-xs mr-2">
+                <textarea value={description} onChange={(e) => setDescription(e.target.value)} className="textarea textarea-bordered max-w-xs h-24"></textarea>
+                <label className="label">
+                  <span className="label-text-alt">Description</span>
+                </label>
+              </div>
+              <div>
+                <button className="btn">
+                  Upload Cover Photo
+                </button>
+              </div>
+            </div>
+            <div className='sm:flex'>
+              <div className="form-control">
+                <label className="label cursor-pointer">
+                  <span className="label-text mr-2">Allow registration</span>
+                  <input type="checkbox" className="checkbox" checked={registration} onChange={(e) => setRegistration(e.target.checked)} />
+                </label>
+              </div>
+            </div>
+            <div className={`${registration ? "" : "hidden"}`}>
+              <div className={`sm:flex`}>
+                <div className="form-control w-full max-w-xs mr-2">
+                  <input value={registrationDatetime} onChange={(e) => setRegistrationDatetime(e.target.value)} required type="datetime-local" className="input input-bordered w-full max-w-xs" />
+                  <label className="label">
+                    <span className="label-text-alt">Registration opens</span>
+                  </label>
+                </div>
+                <div className="form-control w-full max-w-xs mr-2">
+                  <input value={signupSize} onChange={(e) => setSignupSize(e.target.valueAsNumber)} required type="text" className="input input-bordered w-full max-w-xs" />
+                  <label className="label">
+                    <span className="label-text-alt">Registration Maximum</span>
+                  </label>
+                </div>
+                <div className="form-control w-full max-w-xs">
+                  <input value={waitlistSize} onChange={(e) => setWaitlistSize(e.target.valueAsNumber)} required type="text" className="input input-bordered w-full max-w-xs" />
+                  <label className="label">
+                    <span className="label-text-alt">Waitlist Maximum</span>
+                  </label>
+                </div>
+              </div>
+            </div>
+            <div>
+              <input type="submit" value="Submit" className="btn btn-primary sm:float-right" onClick={insert} />
+            </div>
+          </div>
+        </form>
       </main>
     </div>
   )
