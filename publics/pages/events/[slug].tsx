e170014bb6faa70e90ed234e17164fae61b9302d
Divya Wagh <divyawagh07@gmail.com>
2022-10-29 15:27:13 -0500

    updated detail page

--- publics/pages/events/[slug].tsx
+++ publics/pages/events/[slug].tsx
@@ -4,11 +4,15 @@ import { GetServerSideProps } from 'next'
 export const getServerSideProps: GetServerSideProps = async (context) => {
     const { data, error } = await supabase
         .from("events")
-        .select(`name, description, event_datetime, registration_datetime, registration, capacity, waitlist_size`)
+        .select(`name, description, event_datetime, registration_datetime, registration, capacity, waitlist_size, organization (
+            name,
+            photo
+        )`)
         .eq("slug", context.params.slug)
         .single()
 
     if (error) throw (error)
+    console.log(data)
 
     // if no event is found, redirect to 404 page
     if (data === null) {
@@ -29,6 +33,12 @@ type EventDetail = {
     registration: boolean
     capacity: number
     waitlist_size: number
+    organization: OrganizationDetail
+}
+
+type OrganizationDetail = {
+    name: string,
+    photo: string
 }
 
 type Props = {
@@ -39,13 +49,35 @@ const details = (props: Props) => {
     const event = props.data
 
     // process datetimes
+    const weekday = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    const month = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
     event.registration_datetime = new Date(event.registration_datetime)
     event.event_datetime = new Date(event.event_datetime)
+    console.log(event)
 
     return (
         <div>
             <main>
                 <div className="hero min-h-screen bg-base-200">
+                    <div className="hero-content flex-col lg:flex-row">
+                        <img src="https://visit-oxford.s3.amazonaws.com/wp-content/uploads/2018/05/16180528/pum.jpg" className="max-w-sm rounded-lg shadow-2xl" />
+                        <div>
+                            <h1 className="text-5xl font-bold">{event.name}</h1>
+                            <p className="text-xl">{weekday[event.event_datetime.getDay()] + ", " + month[event.event_datetime.getMonth()] + " " + event.event_datetime.getDate() + "th"} </p>
+
+                            <div style = display: flex, justify-content: space-between>
+                            <img className="object-scale-down h-5 w-5" src={event.organization.photo} />
+                            <p className="">Hosted by {event.organization.name}</p>
+                        </div>
+
+                        <p className="py-6">Description: {event.description}</p>
+                        <button className="btn btn-primary">Get Started</button>
+                    </div>
+                </div>
+        </div >
+
+                {/* <div className="hero min-h-screen bg-base-200">
                     <div className="hero-content">
                         <div className="max-w-md">
                             <h1 className="text-5xl font-bold">{event.name} Details!</h1>
@@ -63,9 +95,9 @@ const details = (props: Props) => {
                             </div>
                         </div>
                     </div>
-                </div>
-            </main>
-        </div>
+                </div> */}
+            </main >
+        </div >
     )
 }
 
