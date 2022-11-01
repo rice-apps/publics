c57c1a079418e502419d3e2937a83c73bda42176
Evan-Stegall <ers11@rice.edu>
2022-10-31 19:53:17 -0500

    fixed api to match up with current DB schema

--- publics/pages/api/events/create.ts
+++ publics/pages/api/events/create.ts
@@ -6,31 +6,28 @@ export default withApiAuth(async function ProtectedRoute(
     supabaseServerClient
 ) {
     const body = req.body;
-
+    
     const fields = [
         "name", 
-        "created_at", 
         "capacity", 
         "signup_size", 
         "waitlist_size",
-        "admins", 
-        "counters", 
+        //admins?
+        //counters? 
         "description", 
         "event_datetime", 
         "registration_datetime",    
-        "registration"
+        "registration",
+        //"organization",
+        "location",
     ];
 
-    //Send an error if there are more/less elements in the data than we can put into the table
-    if (Object.keys(body).length != fields.length) {
-        return res.status(400).json({data : "Data not in correct format; Number of fields is incorrect"})
-    }
     //Send an error if a field is missing
-    for (let field in fields) {
-        if (!body[field]) {
+    fields.forEach((field) => {
+        if(!body[field]) {
             return res.status(400).json({data : `${field} not Found!`})
         }
-    }
+    })
 
     //Writing to the DB
     const {data, error} = await supabaseServerClient.from("events").insert(body).single();
