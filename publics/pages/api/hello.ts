201bccc2ef367300017633bd6d5e766c359de92c
AG <agentaidan@gmail.com>
2022-11-02 18:28:22 -0500

    fix api edge config

--- publics/pages/api/hello.ts
+++ publics/pages/api/hello.ts
@@ -5,6 +5,11 @@ type Data = {
   name: string
 }
 
+export const config = {
+  runtime: 'experimental-edge',
+}
+
+
 export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
