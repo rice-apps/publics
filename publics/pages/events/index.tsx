408c439ad1411ddb99480f4edf0e81c169bd9cb2
ey91 <ey9@rice.edu>
2022-11-05 15:48:01 -0500

    added details of event list, wip date formatting of myevents

--- publics/pages/events/index.tsx
+++ publics/pages/events/index.tsx
@@ -3,28 +3,42 @@ import { supabase } from "../../utils/db"
 import type { ListEvent } from "../../utils/types"
 
 type Props = {
-    data: ListEvent[]
+    eventList: ListEvent[]
 }
 
 function Events(props: Props) {
     return (
-        <div className="grid grid-cols-4 gap-4">
-            {props.data.map((event) => <EventCard event={event}/>)}
-            
-        </div>
+        <>
+            <div className="grid grid-cols-2">
+                {/* this one */}
+            </div>
+            <div className="divider">OR</div>
+            <div className="grid grid-cols-4 gap-4">
+                {props.eventList.map((event) => <EventCard event={event} />)}
+            </div>
+        </>
     )
 }
-  
+
+
 // This gets called on every request
 export async function getServerSideProps() {
     const { data, error } = await supabase
-    .from('events')
-    .select(`name, description, slug`)
+        .from('events')
+        .select(`name, description, event_datetime, slug, organization (name, photo, id), registration, registration_datetime`)
     if (error) {
         throw error
     }
-    return {props: {data}} // will be passed to the page component as props
-    
+    let props: any = {}
+    props.eventList = data
+
+    // const { data, error } = await supabase
+    //     .from('registrations')
+    //     .select()
+    //     .eq('person', supabase.auth.getUser().id)
+
+    return { props } // will be passed to the page component as props
+
 }
 
 export default Events
