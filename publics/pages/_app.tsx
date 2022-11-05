96517113cac29ea71416d06cd3dd5238a7d478ad
AG <agentaidan@gmail.com>
2022-11-05 13:33:10 -0500

    fix layout name

--- publics/pages/_app.tsx
+++ publics/pages/_app.tsx
@@ -1,6 +1,6 @@
 import '../styles/global.css'
 import type { AppProps } from 'next/app'
-import Layout from '../components/layout'
+import Layout from '../components/Layout'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
