c6e34019ed70543d8c5a275daf8c7351c7b30edd
AG <agentaidan@gmail.com>
2022-11-05 13:58:11 -0500

    updated to next 13

--- publics/pages/index.tsx
+++ publics/pages/index.tsx
@@ -1,7 +1,6 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Link from 'next/link'
-import Image from 'next/image'
 
 const Home: NextPage = () => {
   return (
@@ -15,11 +14,13 @@ const Home: NextPage = () => {
       <main>
         <div className="hero min-h-screen bg-base-200">
           <div className="hero-content flex-col lg:flex-row-reverse">
-            <Image src="/martelpic.jpg" className="max-w-sm rounded-lg shadow-2xl" />
+            <img src="/martelpic.jpg" className="max-w-sm rounded-lg shadow-2xl" />
             <div>
               <h1 className="text-5xl font-bold">Fun!</h1>
               <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-              <Link className="btn btn-primary" href="/events">View Events</Link>
+              <button className='btn btn-primary'>
+                <Link href="/events">View Events</Link>
+              </button>
             </div>
           </div>
         </div>
