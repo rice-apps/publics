6f87bb3dc2eb86f13b93dbd747b92248022de0c0
spoken-angus <angusjelinek@gmail.com>
2022-11-06 13:20:45 -0600

    typing

--- publics/pages/account.tsx
+++ publics/pages/account.tsx
@@ -4,7 +4,7 @@ import Auth from '../components/Auth'
 import Account from '../components/Account'
 import { Session } from '@supabase/supabase-js'
 
-export default function Home({ session }) {
+export default function Home({ session: Session }) {
 
   return (
     <div className="container" style={{ padding: '50px 0 100px 0' }}>
