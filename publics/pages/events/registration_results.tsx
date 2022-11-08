f67696d1aad69cb1efda7d3f13c0cb6142434dd4
Evan-Stegall <ers11@rice.edu>
2022-11-08 16:46:02 -0600

    added basic skeleton

---
+++ publics/pages/events/registration_results.tsx
@@ -0,0 +1,67 @@
+type EventDetails = {
+    eventName: string,
+}
+function ResultPage(props: EventDetails) {
+    return (
+        <div key = "registration_results_page">
+            <div key = "event_title">
+                <h1>{props.eventName}: Event Results</h1>
+            </div>
+            <div className="btn-group btn-group-vertical lg:btn-group-horizontal">
+                <button className="btn">Copy Emails</button>
+                <button className="btn">Add Attendee</button>
+            </div>
+            <div className="overflow-x-auto">
+            <table className="table table-compact w-full">
+                <thead>
+                <tr>
+                    <th></th> 
+                    <th>Date and Time</th> 
+                    <th>First Name</th> 
+                    <th>Last Name</th> 
+                    <th>Email Address</th> 
+                    <th>NetID</th> 
+                    <th>Residentail Colleger</th>
+                    <th>Wristband?</th>
+                </tr>
+                </thead> 
+                <tbody>
+                <tr>
+                    <th></th> 
+                    <td>10/19/2022 12:00PM</td> 
+                    <td>Rebecca</td> 
+                    <td>Yee</td> 
+                    <td>rmy3@rice.edu</td> 
+                    <td>rmy3</td> 
+                    <td>Wiess</td>
+                    <td><input type="checkbox" className="checkbox checkbox-primary" /></td>
+                </tr>
+                </tbody> 
+                <tfoot>
+                <tr>
+                    <th></th> 
+                    <th>Date and Time</th> 
+                    <th>First Name</th> 
+                    <th>Last Name</th> 
+                    <th>Email Address</th> 
+                    <th>NetID</th> 
+                    <th>Residentail Colleger</th>
+                    <th>Wristband?</th>
+                </tr>
+                </tfoot>
+            </table>
+            </div>
+        </div>
+    );
+}
+
+//Used for testing purposes
+function Page() {
+    return (
+        <div>
+            <ResultPage eventName={"Sid 80's"}></ResultPage>
+        </div>
+    );
+}
+
+export default Page;
