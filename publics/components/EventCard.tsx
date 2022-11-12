0ba9fd5fea50b2154decbc290e95f46a9b1b2203
AG <agentaidan@gmail.com>
2022-11-12 15:46:37 -0600

    fix pr

--- publics/components/EventCard.tsx
+++ publics/components/EventCard.tsx
@@ -1,64 +1,39 @@
 import type { ListEvent } from "../utils/types"
 import Link from 'next/link'
+import { registrationOpen } from "../utils/registration"
 
 type Props = {
     event: ListEvent
 }
 
-function registrationOpen(event: ListEvent) {
-    return (event.registration && new Date(event.event_datetime) < new Date() && new Date(event.registration_datetime) >= new Date())
-}
 function getOrdinal(n: string) {
     let ord = ["st", "nd", "rd"]
     let exceptions = [11, 12, 13]
     let newNum = Number(n.replace(/\s/g, ''))
-    console.log(newNum)
     let nth = ord[(newNum % 10) - 1] == undefined || exceptions.includes(newNum % 100) ? "th" : ord[(newNum % 10) - 1]
     return newNum.toString() + nth
 }
 
 function eventCardDate(datetime: string, registration: boolean) {
     const dt: Date = new Date(datetime)
-    const dayNames = [
-        'Sunday',
-        'Monday',
-        'Tuesday',
-        'Wednesday',
-        'Thursday',
-        'Friday',
-        'Saturday'
-    ]
-    const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
-    
-    var weekday = dayNames[dt.getDay()]
-    var month = months[dt.getMonth()]
-    var day = dt.getDate().toLocaleString
-    
-    var date = dt.toLocaleDateString('en-us', { weekday:"long", month:"long", day: "numeric"});
-    var time = dt.toLocaleString('en-US', { hour: 'numeric', minute: 'numeric', hour12: true })
+
+    const date = dt.toLocaleDateString('en-us', { weekday: "long", month: "long", day: "numeric" });
+    const time = dt.toLocaleString('en-US', { hour: 'numeric', minute: 'numeric', hour12: true })
     //console.log(date.slice(date.length-2, date.length-1))
-    var ordinalday = getOrdinal(date.slice(date.length-2))
+    const ordinalday = getOrdinal(date.slice(date.length - 2))
 
-    if (registration)
-    {
-        return `${date.slice(0, date.length-2)} ${ordinalday} @ ${time}`
+    if (registration) {
+        return `${date.slice(0, date.length - 2)} ${ordinalday} @ ${time}`
     }
-    else
-    {
-        return `${date.slice(0, date.length-2)} ${ordinalday}`
+    else {
+        return `${date.slice(0, date.length - 2)} ${ordinalday}`
     }
-    
-    
-   
-
 }
 
 
 export default function EventCard(props: Props) {
-    console.log(props.event)
-    console.log(" datetime:" + props.event.event_datetime)
     return (
-        <div className="card w-full bg-base-100 shadow-xl hover:scale-110 transition-transform">
+        <div className="card w-full bg-base-100 shadow-xl hover:scale-110 transition-transform border border-primary">
             <div className="card-body">
                 <h2 className="card-title">{props.event.name}</h2>
                 <p>{`${eventCardDate(props.event.event_datetime, false)}`} </p>
@@ -68,7 +43,7 @@ export default function EventCard(props: Props) {
                 <p className="font-medium">{!props.event.registration ? 'No registration required' : registrationOpen(props.event) ? 'Registration open' : `Registration opens: ${eventCardDate(props.event.registration_datetime, true)}`}</p>
                 <div className="card-actions justify-end">
                     <button className="btn btn-primary">
-                        <Link href={`events/${props.event.slug}`}>Event Details</Link>
+                        <Link href={`/events/${props.event.slug}`}>Event Details</Link>
                     </button>
                 </div>
             </div>
---
+++ publics/utils/registration.ts
@@ -0,0 +1,5 @@
+import type { ListEvent } from "./types"
+
+export function registrationOpen(event: ListEvent) {
+    return (event.registration && new Date(event.event_datetime) < new Date() && new Date(event.registration_datetime) >= new Date())
+}
