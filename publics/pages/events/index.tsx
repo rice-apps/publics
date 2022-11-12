9e5823d6fe9caf082bc25c4d27fe2ad245c8267b
AG <agentaidan@gmail.com>
2022-11-12 15:46:37 -0600

    fix pr

--- publics/pages/events/index.tsx
+++ publics/pages/events/index.tsx
@@ -8,18 +8,11 @@ type Props = {
 
 function Events(props: Props) {
     return (
-        <>
-            <div className="grid grid-cols-2">
-                {/* this one */}
-            </div>
-            <div className="divider"></div>
-            <div className="px-8 grid grid-cols-3 gap-4">
-                {props.eventList.map((event) => <EventCard event={event} />)}
-            </div>
-        </>
+        <div className="px-8 grid grid-cols-3 gap-4">
+            {props.eventList.map((event) => <EventCard event={event} />)}
+        </div>
     )
 }
-
 
 // This gets called on every request
 export async function getServerSideProps() {
@@ -29,8 +22,7 @@ export async function getServerSideProps() {
     if (error) {
         throw error
     }
-    let props: any = {}
-    props.eventList = data
+    let props: Props = { eventList: data }
 
     // const { data, error } = await supabase
     //     .from('registrations')
@@ -38,7 +30,6 @@ export async function getServerSideProps() {
     //     .eq('person', supabase.auth.getUser().id)
 
     return { props } // will be passed to the page component as props
-
 }
 
 export default Events
