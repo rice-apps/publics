a8ec36c7b073706f83914176637cb82e4ea8d517
ey91 <ey9@rice.edu>
2022-11-12 13:15:53 -0600

    Reformatting of Grid

---
+++ publics/components/MyEventCard.tsx
@@ -0,0 +1,29 @@
+import type { ListEvent } from "../utils/types"
+import Link from 'next/link'
+
+type Props = {
+    event: ListEvent
+}
+
+
+
+export default function EventCard(props: Props) {
+    console.log(props.event)
+    console.log(props.event.event_datetime)
+    return (
+        <div className="card w-full bg-base-100 shadow-xl hover:scale-110 transition-transform">
+            <div className="card-body">
+                <h2 className="card-title">{props.event.name}</h2>
+                <p>{new Date(props.event.event_datetime).toDateString()} </p>
+                <p className="flex"><img className="w-8 mr-2" src={props.event.organization.photo} alt={props.event.organization.name}></img>{props.event.organization.name}</p>
+                <p>{!props.event.registration ? 'No registration required' : registrationOpen(props.event) ? 'Registration open' : `Registration opens ${new Date(props.event.registration_datetime).getDay()}`}</p>
+                <div className="card-actions justify-end">
+                    <button className="btn btn-primary">
+                        <Link href={`events/${props.event.slug}`}>Event Details</Link>
+                    </button>
+                </div>
+            </div>
+        </div>
+    )
+
+}
