60c77d137264099d2f8aef42e1e3aa2dbf566af1
Evan-Stegall <ers11@rice.edu>
2022-11-13 23:27:08 -0600

    can pull registration data and organize it into table

--- publics/pages/events/registration_results.tsx
+++ publics/pages/events/registration_results.tsx
@@ -1,7 +1,93 @@
+import { supabase } from '../../utils/db'
+import { GetServerSideProps } from 'next'
+import { render } from 'react-dom';
+import { useEffect, useState } from 'react';
+
 type EventDetails = {
     eventName: string,
 }
+
+interface rowObject {
+    created_at : string,
+    first_name : string,
+    last_name : string,
+    email : string,
+    netid : string,
+    college: string,
+}
+
 function ResultPage(props: EventDetails) {
+ 
+    const [loading, setLoading] = useState(true)
+    const [registrations, setRegistrations] = useState<rowObject[]>([]);
+    const [event, setEvent] = useState("");
+
+    //Gets the public that the user accessing this page is an admin of
+    async function getPublic() {
+        //TODO populate this with data that uses the auth of the person using this site
+        //Find the event that this person is an admit of and return it
+        return 'afdbe8da-f2cd-47a2-848c-6c5aa891b881';
+    };
+    //Gets the set of registrations for the given event
+    async function getRegistrations(event: string) {
+        const {data, error} = await supabase.
+        from("registrations")
+        .select(`
+            person,
+            created_at,
+            profiles (
+                id,
+                first_name,
+                last_name,
+                college,
+                netid
+            )
+        `)
+        .eq('event', event)
+        
+        let formatted_data: rowObject[] = []
+
+        if (error) {
+            //Throw bigger error
+            console.log(error)
+            return formatted_data;
+        }
+        
+        for(var i = 0; i < data.length; i++) {
+            var current_object = data[i];
+            var profiles = current_object["profiles"] as Object;
+            if (profiles == null) {
+                return []
+            }
+            //TODO fix this, typescript for some reason doesn't think that I can index the profiles object with the given keys
+            //ERROR Element implicitly has an 'any' type because expression of type '"created_at"' can't be used to index type 'Object'. Property 'created_at' does not exist on type 'Object'
+            var formatted_object = {
+                "created_at" : new Date(current_object["created_at"]).toLocaleString(),
+                "first_name" : Object.values(profiles)[1],
+                "last_name" : Object.values(profiles)[2],
+                "email" : Object.values(profiles)[4] + "@rice.edu",
+                "netid" :  Object.values(profiles)[4],
+                "college" : Object.values(profiles)[3],
+            }
+
+            formatted_data[i] = formatted_object;
+        }
+
+        return formatted_data;
+    }; 
+
+    //Getting the public
+    getPublic().then((value) => {
+        if(value != null) {
+            getRegistrations(value).then((value) => {
+                setLoading(false)
+                setRegistrations(value)
+            })
+        }
+    })
+
+    if (loading) return <div>Loading...</div>
+
     return (
         <div key = "registration_results_page">
             <div key = "event_title">
@@ -21,21 +107,25 @@ function ResultPage(props: EventDetails) {
                     <th>Last Name</th> 
                     <th>Email Address</th> 
                     <th>NetID</th> 
-                    <th>Residentail Colleger</th>
+                    <th>Residential College</th>
                     <th>Wristband?</th>
                 </tr>
                 </thead> 
                 <tbody>
-                <tr>
-                    <th></th> 
-                    <td>10/19/2022 12:00PM</td> 
-                    <td>Rebecca</td> 
-                    <td>Yee</td> 
-                    <td>rmy3@rice.edu</td> 
-                    <td>rmy3</td> 
-                    <td>Wiess</td>
-                    <td><input type="checkbox" className="checkbox checkbox-primary" /></td>
-                </tr>
+                {
+                        registrations.map((row, index) => {
+                            return <tr key = {index}>
+                            <th></th>
+                            <td>{row["created_at"]}</td>
+                            <td>{row["first_name"]}</td>
+                            <td>{row["last_name"]}</td>
+                            <td>{row["email"]}</td>
+                            <td>{row["netid"]}</td>
+                            <td>{row["college"]}</td>
+                            <td><input type="checkbox" className="checkbox checkbox-primary" /></td>
+                        </tr>
+                        })
+                }
                 </tbody> 
             </table>
             </div>
