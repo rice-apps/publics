c05f394321e69a6e46a57434dde4eeb66b14dd1a
spoken-angus <angusjelinek@gmail.com>
2022-11-16 09:11:55 -0600

    added public theme

--- publics/components/Navbar.tsx
+++ publics/components/Navbar.tsx
@@ -6,7 +6,7 @@ export default function Navbar() {
     useEffect(() => {
         themeChange(false)
     }, [])
-    const themes: string[] = ["light", "dark", "cupcake", "bumblebee", "emerald", "corporate", "synthwave", "retro", "cyberpunk", "valentine", "halloween", "garden", "forest", "aqua", "lofi", "pastel", "fantasy", "wireframe", "black", "luxury", "dracula", "cmyk", "autumn", "business", "acid", "lemonade", "night", "coffee", "winter"];
+    const themes: string[] = ["publics", "light", "dark", "cupcake", "bumblebee", "emerald", "corporate", "synthwave", "retro", "cyberpunk", "valentine", "halloween", "garden", "forest", "aqua", "lofi", "pastel", "fantasy", "wireframe", "black", "luxury", "dracula", "cmyk", "autumn", "business", "acid", "lemonade", "night", "coffee", "winter"];
 
     const navbar_content = (<><li><Link href="/events">Events</Link></li>
         <li tabIndex={0}>
