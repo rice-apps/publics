65dce087383a8e5ceb9a355ba19a449581c45c83
spoken-angus <angusjelinek@gmail.com>
2022-11-16 16:19:10 -0600

    sticky footer

--- publics/components/Layout.tsx
+++ publics/components/Layout.tsx
@@ -3,10 +3,10 @@ import Footer from './Footer';
 
 export default function Layout({ children }: { children: React.ReactNode }) {
     return (
-        <>
-            <Navbar />
-            <main>{children}</main>
+        <div className="flex flex-col h-screen justify-between">
+            <Navbar className="h-24"/>
+            <main className="mb-auto">{children}</main>
             <Footer />
-        </>
+        </div>
     )
 }
--- publics/components/Navbar.tsx
+++ publics/components/Navbar.tsx
@@ -22,7 +22,7 @@ export default function Navbar() {
         <li><a>Item 3</a></li></>)
 
     return (
-        <div className="navbar bg-base-100">
+        <div className="navbar bg-base-100 min-h-fit">
             <div className="navbar-start">
                 <div className="dropdown">
                     <label tabIndex={0} className="btn btn-ghost lg:hidden">
