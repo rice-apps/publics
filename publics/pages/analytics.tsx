50e76852f108a845f39512e1da0ae5eba452443f
Gabby Franklin <70460191+fgabrielle1445@users.noreply.github.com>
2022-11-19 13:49:47 -0600

    Notated more analytics to add

--- publics/pages/analytics.tsx
+++ publics/pages/analytics.tsx
@@ -171,6 +171,10 @@ export default function Analytics() {
     // 4. Number of volunteers
     // 5. Number of volunteers in an hour (registered v. showed up)
     // 6. [This one depends on some serious checking] caregiving rate in/rate out
+    // 7. Registration analytics
+        // - Number of registrants: residential college hosting and general registration
+        // - Registered versus pick up
+        // - Chances of getting off the waitlist
 
   return (
     <div></div>
