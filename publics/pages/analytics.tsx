19c78e285a60c2a0416c461d078163daa09948fb
Gabby Franklin <70460191+fgabrielle1445@users.noreply.github.com>
2022-11-19 13:47:52 -0600

    Making a list of analytics we could need

--- publics/pages/analytics.tsx
+++ publics/pages/analytics.tsx
@@ -163,6 +163,15 @@ return (
 =======
 
 export default function Analytics() {
+    // Going to need to calculate:
+
+    // 1. Total people counted
+    // 2. In and out by hour
+    // 3. In and out by volunteer
+    // 4. Number of volunteers
+    // 5. Number of volunteers in an hour (registered v. showed up)
+    // 6. [This one depends on some serious checking] caregiving rate in/rate out
+
   return (
     <div></div>
   );
