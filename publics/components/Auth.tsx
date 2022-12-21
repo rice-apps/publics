948c94109ccf9c09046855dbbedeb1fc918669cf
spoken-angus <angusjelinek@gmail.com>
2022-12-20 18:06:42 -0600

    fixed auth redirect for vercel and production preview

--- publics/components/Auth.tsx
+++ publics/components/Auth.tsx
@@ -5,13 +5,25 @@ export default function Auth() {
   const [loading, setLoading] = useState(false)
   const [email, setEmail] = useState('')
 
+  const getURL = () => {
+    let url =
+      process?.env?.NEXT_PUBLIC_SITE_URL ?? // Set this to your site URL in production env.
+      process?.env?.NEXT_PUBLIC_VERCEL_URL ?? // Automatically set by Vercel.
+      'http://localhost:3000/';
+    // Make sure to include `https://` when not localhost.
+    url = url.includes('http') ? url : `https://${url}`;
+    // Make sure to including trailing `/`.
+    url = url.charAt(url.length - 1) === '/' ? url : `${url}/`;
+    return url;
+  };
+
   const handleLogin = async () => {
     const res = await supabase.auth.signInWithOAuth({
-      provider: 'google'
+      provider: 'google',
+      options: {
+        redirectTo: getURL()
+      }
     },
-      // {
-      //   redirectTo: 'https://example.com/welcome'
-      // }
     )
     console.log(res)
   }
