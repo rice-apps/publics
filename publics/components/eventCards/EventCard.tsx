fcbb2d65832736c121d9fb8603c3c7fa680c8ccd
spoken-angus <angusjelinek@gmail.com>
2022-12-26 15:25:29 -0500

    removed duplicate code

--- publics/components/eventCards/EventCard.tsx
+++ publics/components/eventCards/EventCard.tsx
@@ -1,35 +1,11 @@
 import type { ListEvent } from "../../utils/types"
 import Link from 'next/link'
 import { registrationOpen } from "../../utils/registration"
+import { eventCardDate } from "./cardDate"
 
 type Props = {
     event: ListEvent
 }
-
-export function getOrdinal(n: string) {
-    let ord = ["st", "nd", "rd"]
-    let exceptions = [11, 12, 13]
-    let newNum = Number(n.replace(/\s/g, ''))
-    let nth = ord[(newNum % 10) - 1] == undefined || exceptions.includes(newNum % 100) ? "th" : ord[(newNum % 10) - 1]
-    return newNum.toString() + nth
-}
-
-export function eventCardDate(datetime: string, registration: boolean) {
-    const dt: Date = new Date(datetime)
-
-    const date = dt.toLocaleDateString('en-us', { weekday: "long", month: "long", day: "numeric" });
-    const time = dt.toLocaleString('en-US', { hour: 'numeric', minute: 'numeric', hour12: true })
-    //console.log(date.slice(date.length-2, date.length-1))
-    const ordinalday = getOrdinal(date.slice(date.length - 2))
-
-    if (registration) {
-        return `${date.slice(0, date.length - 2)} ${ordinalday} @ ${time}`
-    }
-    else {
-        return `${date.slice(0, date.length - 2)} ${ordinalday}`
-    }
-}
-
 
 export default function EventCard(props: Props) {
     return (
