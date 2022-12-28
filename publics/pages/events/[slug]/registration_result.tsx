3426171b76401b5b302ee3a6be0417dc51803283
nram1 <90943799+nram1@users.noreply.github.com>
2022-12-27 18:50:22 -0600

    error msgs added

--- publics/pages/events/[slug]/registration_result.tsx
+++ publics/pages/events/[slug]/registration_result.tsx
@@ -255,10 +255,27 @@ function ResultPage(props) {
 
             //refresh page
             setRegistration(await getRegistrations(eventDetails!));
+            return(
+                <div className="alert alert-success shadow-lg">
+                    <div>
+                        <svg xmlns="http://www.w3.org/2000/svg" className="stroke-current flex-shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
+                        <span>User successfully added!</span>
+                    </div>
+                </div>
+            )
+
 
         } else {
             console.log("Got error")
             console.log(error)
+            return(
+                <div className="alert alert-error shadow-lg">
+                    <div>
+                        <svg xmlns="http://www.w3.org/2000/svg" className="stroke-current flex-shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
+                        <span>Error! User already added or does not exist.</span>
+                    </div>
+                </div>
+            )
         }
 
     }
@@ -278,6 +295,24 @@ function ResultPage(props) {
         if (!res) {
             console.log("ERROR in removing attendee")
             console.log(res)
+            return(
+                <div className="alert alert-error shadow-lg">
+                    <div>
+                        <svg xmlns="http://www.w3.org/2000/svg" className="stroke-current flex-shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
+                        <span>Error! User not in database.</span>
+                    </div>
+                </div>
+            )
+        }
+        else{
+            return(
+                <div className="alert alert-success shadow-lg">
+                    <div>
+                        <svg xmlns="http://www.w3.org/2000/svg" className="stroke-current flex-shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
+                        <span>User successfully removed!</span>
+                    </div>
+                </div>
+            )
         }
 
         //refresh page
