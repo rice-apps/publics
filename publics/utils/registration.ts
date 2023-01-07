301bc270cb1aa2e6e0b9af641b2b2b09fdd89648
spoken-angus <angusjelinek@gmail.com>
2023-01-07 18:33:32 -0500

    pagination and search for registrations

--- publics/utils/registration.ts
+++ publics/utils/registration.ts
@@ -3,3 +3,11 @@ import type { ListEvent } from "./types"
 export function registrationOpen(event: ListEvent) {
     return event.registration && new Date(event.event_datetime) > new Date() && new Date(event.registration_datetime) <= new Date()
 }
+
+export const getPagination = (page, size) => {
+    const limit = size ? +size : 3
+    const from = page ? page * limit : 0
+    const to = page ? from + size - 1 : size - 1
+  
+    return { from, to }
+  }
