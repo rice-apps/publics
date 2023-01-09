40b4f50f1352cd8257109c00ca1b34dff46a2f27
Evan-Stegall <evanstegall007@gmail.com>
2023-01-09 13:48:58 -0600

    rebase

--- publics/pages/events/[slug]/shifts/[shift].tsx
+++ publics/pages/events/[slug]/shifts/[shift].tsx
@@ -68,10 +68,15 @@ function VolunteerPage(props) {
     //netID of user to add to registration table, used with the add attendee button
     const [netID, setNetID] = useState("");
 <<<<<<< HEAD
+<<<<<<< HEAD
     //shift id taken from the shift slug
     const [shift, setShift] = useState<String>("");
 =======
 >>>>>>> 9c58da4 (add volunteer now creates a row with a null shift)
+=======
+    //shift id taken from the shift slug
+    const [shift, setShift] = useState<String>("");
+>>>>>>> 98aab8c (added functionaliy for adding/removing attendents and cleaned code)
     const [filterByAll, setFilterByAll] = useState(true); //starts as true as we want to start by initially showing the admin the entire set of registered users
     const [filterByCheckedIn, setFilterByCheckedIn] = useState(false);
     const [filterByCounter, setFilterByCounter] = useState(false);
@@ -93,14 +98,20 @@ function VolunteerPage(props) {
         const registrations = await getRegistrations(event_detail);
 
 <<<<<<< HEAD
+<<<<<<< HEAD
         const shift_id = await getShift(event_detail);
 =======
 >>>>>>> 9c58da4 (add volunteer now creates a row with a null shift)
+=======
+        const shift_id = await getShift(event_detail);
+
+>>>>>>> 98aab8c (added functionaliy for adding/removing attendents and cleaned code)
         // //Set event details
         setEventDetails(event_detail);
         // //Set registrations
         setRegistration(registrations);
         // set shift id
+        setShift(shift_id);
         //Stop loading
         setLoading(false);
     }
@@ -271,6 +282,22 @@ function VolunteerPage(props) {
         navigator.clipboard.writeText(emails!.join(' '))
     }
 
+    async function getShift(event_detail: EventDetails): Promise<String> {
+        const {data, error} = await supabase
+        .from("shifts")
+        .select("id")
+        .eq("event", event_detail.eventID)
+        .eq("slug", props.params.shift)
+        .single();
+
+        if (error) {
+            console.log("ERROR");
+            router.push("/404");
+        }
+
+        return data?.id;
+    }
+
     /**
      * Registers attendee to this event given an inputted netID
      * @param netID - you know what this is
@@ -321,7 +348,7 @@ function VolunteerPage(props) {
             //Insert person into registrations table for this event
             const res = await supabase
             .from("volunteers")
-            .insert({"event" : eventDetails!.eventID, "profile": personID})
+            .insert({"event" : eventDetails!.eventID, "profile": personID, "shift" : shift})
             .select();
 
             console.log(eventDetails!.eventID)
@@ -336,6 +363,7 @@ function VolunteerPage(props) {
         }
 
     }
+
 
     /**
      * Unregisters attendee from this event. Updates backend and refreshes page
