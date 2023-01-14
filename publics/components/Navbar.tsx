316de1161333cfdb3c1eb6e5b7381aa5832130cc
AG <agentaidan@gmail.com>
2023-01-14 13:38:35 -0600

    remove unused navbar items

--- publics/components/Navbar.tsx
+++ publics/components/Navbar.tsx
@@ -2,19 +2,12 @@ import { useEffect } from 'react'
 import Link from 'next/link'
 
 export default function Navbar({ session }) {
-    const navbar_content = (<><li><Link href="/events">Events</Link></li>
-        <li><Link href="mailto:awj3@rice.edu">Contact</Link></li>
-        <li tabIndex={0}>
-            <a className="justify-between">
-                Parent
-                <svg className="fill-current" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M8.59,16.58L13.17,12L8.59,7.41L10,6L16,12L10,18L8.59,16.58Z" /></svg>
-            </a>
-            <ul className="p-2">
-                <li><a>Submenu 1</a></li>
-                <li><a>Submenu 2</a></li>
-            </ul>
-        </li>
-        <li><a>Item 3</a></li></>)
+    const navbar_content = (
+        <>
+            <li><Link href="/events">Events</Link></li>
+            <li><Link href="mailto:awj3@rice.edu">Contact</Link></li>
+        </>
+    )
 
     return (
         <div className="navbar bg-base-100 min-h-fit">
