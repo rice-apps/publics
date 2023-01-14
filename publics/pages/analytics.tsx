044a8b7261bec8c8a81333cc78963cb2ad4b3759
Gabby Franklin <70460191+fgabrielle1445@users.noreply.github.com>
2023-01-14 13:47:53 -0600

    Setting up counts graph

--- publics/pages/analytics.tsx
+++ publics/pages/analytics.tsx
@@ -1,6 +1,7 @@
 import { supabase } from "../../../utils/db";
 import { useState, useEffect } from "react";
 import { useRouter } from "next/router";
+import { ResponsiveLine } from '@nivo/line';
 
 export default function Analytics(props) {
     // Going to need to calculate:
@@ -18,7 +19,7 @@ export default function Analytics(props) {
     // 7. [This one depends on some serious checking] caregiving rate in/rate out
 
     const { session } = props;
-  const { query } = useRouter() || { query: { slug: "" } };
+    const { query } = useRouter() || { query: { slug: "" } };
 
     const fetchPosts = async () => {
         if (!query.slug) return;
@@ -39,8 +40,82 @@ export default function Analytics(props) {
         const { data: volunteers } = await supabase
           .from("volunteers")
           .select("id, profile(first_name), event(slug)");
+    }
 
-  return (
-    <div></div>
-  );
+    const countsData = fetchPosts;
+
+    // For reference: https://nivo.rocks/line/
+const countsLineGraph = ({ countsData }) => (
+  <ResponsiveLine
+      data={countsData}
+      margin={{ top: 50, right: 110, bottom: 50, left: 60 }}
+      xScale={{ type: 'point' }}
+      yScale={{
+          type: 'linear',
+          min: 'auto',
+          max: 'auto',
+          stacked: true,
+          reverse: false
+      }}
+      yFormat=" >-.2f"
+      axisTop={null}
+      axisRight={null}
+      axisBottom={{
+          orient: 'bottom',
+          tickSize: 5,
+          tickPadding: 5,
+          tickRotation: 0,
+          legend: 'time',
+          legendOffset: 36,
+          legendPosition: 'middle'
+      }}
+      axisLeft={{
+          orient: 'left',
+          tickSize: 5,
+          tickPadding: 5,
+          tickRotation: 0,
+          legend: 'count',
+          legendOffset: -40,
+          legendPosition: 'middle'
+      }}
+      pointSize={10}
+      pointColor={{ theme: 'background' }}
+      pointBorderWidth={2}
+      pointBorderColor={{ from: 'serieColor' }}
+      pointLabelYOffset={-12}
+      useMesh={true}
+      legends={[
+          {
+              anchor: 'bottom-right',
+              direction: 'column',
+              justify: false,
+              translateX: 100,
+              translateY: 0,
+              itemsSpacing: 0,
+              itemDirection: 'left-to-right',
+              itemWidth: 80,
+              itemHeight: 20,
+              itemOpacity: 0.75,
+              symbolSize: 12,
+              symbolShape: 'circle',
+              symbolBorderColor: 'rgba(0, 0, 0, .5)',
+              effects: [
+                  {
+                      on: 'hover',
+                      style: {
+                          itemBackground: 'rgba(0, 0, 0, .03)',
+                          itemOpacity: 1
+                      }
+                  }
+              ]
+          }
+      ]}
+  />
+)
+
+return (
+  <div>
+
+  </div>
+);
 }
