7d0836b54c39fa31e7c54a822d342585863e578a
AG <agentaidan@gmail.com>
2023-01-19 14:24:58 -0600

    fix minor styles

--- publics/components/Auth.tsx
+++ publics/components/Auth.tsx
@@ -30,21 +30,21 @@ export default function Auth() {
       <div className="hero-content flex-col lg:flex-row-reverse">
         <img
           src="https://placeimg.com/260/400/arch"
-          className="max-w-sm rounded-lg shadow-2xl"
+          className="ml-2 max-w-sm rounded-lg shadow-2xl"
         />
-        <div>
+        <div className="lg:max-w-2/5 md:max-w-2/3">
           <h1 className="text-4xl font-bold">
             Streamlining public parties at Rice.
           </h1>
-          <p className="py-6">
+          <p className="py-6 text-xl">
             <span className="text-primary">Attendees</span> can register for
             events in-app and access important event information.
           </p>
-          <p className="py-6">
+          <p className="py-6 text-xl">
             <span className="text-primary">Volunteers</span> can seamlessly
             check in and out of their shift and track event capacity.
           </p>
-          <p className="py-6">
+          <p className="py-6 text-xl">
             <span className="text-primary">Socials</span> can create events,
             manage attendees and volunteers, and view event analytics.
           </p>
