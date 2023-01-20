b20df17b5155cf32e29e30f2bc71381ff4f988ff
spoken-angus <angusjelinek@gmail.com>
2023-01-20 10:42:24 -0600

    stroke fix

--- publics/pages/events/[slug]/shifts/[shift].tsx
+++ publics/pages/events/[slug]/shifts/[shift].tsx
@@ -462,8 +462,8 @@ function VolunteerPage(props) {
                 <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
                 <g
                   id="SVGRepo_tracerCarrier"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
                 ></g>
                 <g id="SVGRepo_iconCarrier">
                   {" "}
