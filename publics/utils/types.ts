08d3998f6586b951c96d9dbb28676c1bd61fdadc
gueyman <63422466+gueyman@users.noreply.github.com>
2023-01-21 14:40:25 -0600

    rebase

--- publics/utils/types.ts
+++ publics/utils/types.ts
@@ -1,19 +1,15 @@
 export type ListEvent = {
-  id: string
-  name: string
-  description: string
-  slug: string
-  event_datetime: string
-  organization: Organization
-  registration: boolean
-  registration_datetime: string
-  college_registration_datetime: string
-  registration_closed: boolean
-  img_url: string
+    id: string
+    name: string
+    description: string
+    slug: string
+    event_datetime: string
+    organization: Organization
+    registration: boolean
+    registration_datetime: string
 }
 
 type Organization = {
-<<<<<<< HEAD
     id: string
     photo: string
     name: string
@@ -25,9 +21,3 @@ export type MemberAbout = {
     title: string
     college: string
 }
-=======
-  id: string
-  photo: string
-  name: string
-}
->>>>>>> main
