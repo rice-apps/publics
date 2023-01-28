c0311beccc532527e1d1a9d4f3b2c709c955acd6
Emma Li <emmali@Emmas-MacBook-Pro.local>
2023-01-28 15:24:40 -0600

    edit shift model (draft, will try to edit add shift modal instead)

---
+++ publics/components/shifts/EditShiftModal.tsx
@@ -0,0 +1,116 @@
+import { useState } from "react"
+import { useSupabaseClient } from "@supabase/auth-helpers-react"
+
+function EditShiftModal(props) {
+  const [name, setName] = useState<string>("")
+  const [shiftStartDateTime, setShiftStartDateTime] = useState<string | number>(
+    ""
+  )
+  const [shiftEndDateTime, setShiftEndDateTime] = useState<string | number>("")
+  const [volunteerInstructions, setVolunteerInstructions] = useState<
+    string | number
+  >("")
+
+  const supabase = useSupabaseClient()
+
+  const createShift = async (e) => {
+    e.preventDefault()
+
+    const { data, error } = await supabase.from("shifts").insert([
+      {
+        name: name,
+        start: new Date(shiftStartDateTime),
+        end: new Date(shiftEndDateTime),
+        volunteer_instructions: volunteerInstructions,
+        event: props.eventId,
+      },
+    ])
+    if (error) {
+      alert(error)
+    } else {
+      window.location.reload()
+    }
+  }
+
+  return (
+    <>
+      <label htmlFor="move-modal" className="btn btn-link">
+        Edit Shift Details
+      </label>
+      <input type="checkbox" id="move-modal" className="modal-toggle" />
+      <div className="modal">
+        <div className="modal-box w-5/6 max-w-3xl">
+          <h3 className="font-bold text-lg">Edit shift details</h3>
+          <form onSubmit={createShift}>
+            <div className="flex flex-row justify-between">
+              <div className="form-control max-w-xs">
+                <label className="label">
+                  <span className="label-text">Type</span>
+                </label>
+                <select
+                  required
+                  className="select select-bordered max-w-xs"
+                  onChange={(e) => setName(e.target.value)}
+                  value={name}
+                >
+                  <option disabled selected value="">
+                    Type
+                  </option>
+                  <option>Capacity Counter</option>
+                  <option>Server</option>
+                  <option>Caregiver</option>
+                </select>
+              </div>
+              <div className="form-control max-w-xs">
+                <label className="label">
+                  <span className="label-text">Start</span>
+                </label>
+                <input
+                  onChange={(e) => setShiftStartDateTime(e.target.value)}
+                  type="datetime-local"
+                  required
+                  className="input input-bordered"
+                />
+              </div>
+              <div className="form-control max-w-xs">
+                <label className="label">
+                  <span className="label-text">End</span>
+                </label>
+                <input
+                  onChange={(e) => setShiftEndDateTime(e.target.value)}
+                  type="datetime-local"
+                  required
+                  className="input input-bordered"
+                />
+              </div>
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Volunteer Instructions</span>
+              </label>
+              <textarea
+                value={volunteerInstructions}
+                onChange={(e) => setVolunteerInstructions(e.target.value)}
+                className="textarea textarea-bordered h-24"
+                placeholder="Instructions..."
+              ></textarea>
+            </div>
+            <div className="modal-action">
+              <label
+                className="btn btn-outline btn-primary"
+                htmlFor="move-modal"
+              >
+                Cancel
+              </label>
+              <button type="submit" className="btn btn-primary">
+                Save
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default EditShiftModal
