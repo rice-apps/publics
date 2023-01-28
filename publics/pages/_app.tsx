28670565d1618d987f7966b8682ba303f454bf6a
AG <agentaidan@gmail.com>
2023-01-28 15:46:25 -0600

    made splash page, trying to fix login

--- publics/pages/_app.tsx
+++ publics/pages/_app.tsx
@@ -2,59 +2,69 @@ import Layout from "../components/Layout"
 import "../styles/global.css"
 import { createBrowserSupabaseClient } from "@supabase/auth-helpers-nextjs"
 import { SessionContextProvider, Session } from "@supabase/auth-helpers-react"
+import type { NextPage } from "next"
 import { DefaultSeo } from "next-seo"
 import type { AppProps } from "next/app"
 import Head from "next/head"
 import { useState } from "react"
+import type { ReactElement, ReactNode } from "react"
 
-function MyApp({
+export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
+  getLayout?: (page: ReactElement) => ReactNode
+}
+
+export default function MyApp({
   Component,
   pageProps,
 }: AppProps<{
   initialSession: Session
-}>) {
+}> & {
+  Component: NextPageWithLayout
+}) {
   const [supabaseClient] = useState(() => createBrowserSupabaseClient())
 
-  return (
-    <>
-      <Head>
-        <meta
-          name="google-site-verification"
-          content="pQJj2FGXHM4uMmvH23WE8XS_7GPJIiCUXMwqc_24wOI"
+  const getLayout =
+    Component.getLayout ||
+    ((page) => (
+      <>
+        <Head>
+          <link rel="icon" href="/owl.svg" />
+          <meta
+            name="google-site-verification"
+            content="pQJj2FGXHM4uMmvH23WE8XS_7GPJIiCUXMwqc_24wOI"
+          />
+        </Head>
+        <DefaultSeo
+          title="PartyOwl"
+          description="PartyOwl streamlines public parties at Rice University with useful features for attendees, volunteers, and socials."
+          openGraph={{
+            type: "website",
+            locale: "en_uS",
+            url: "https://publics.rice.edu/",
+            siteName: "PartyOwl",
+            images: [
+              {
+                url: "https://publics.vercel.app/owl.png",
+                width: 712,
+                height: 512,
+                alt: "Owl",
+              },
+            ],
+          }}
+          twitter={{
+            cardType: "summary_large_image",
+          }}
         />
-        <link rel="icon" href="/owl.svg" />
-      </Head>
-      <DefaultSeo
-        title="PartyOwl"
-        description="PartyOwl streamlines public parties at Rice University with useful features for attendees, volunteers, and socials."
-        openGraph={{
-          type: "website",
-          locale: "en_uS",
-          url: "https://publics.rice.edu/",
-          siteName: "PartyOwl",
-          images: [
-            {
-              url: "https://publics.vercel.app/owl.svg",
-              width: 712,
-              height: 512,
-              alt: "Owl",
-            },
-          ],
-        }}
-        twitter={{
-          cardType: "summary_large_image",
-        }}
-      />
-      <SessionContextProvider
-        supabaseClient={supabaseClient}
-        initialSession={pageProps.initialSession}
-      >
-        <Layout>
-          <Component {...pageProps} />
-        </Layout>
-      </SessionContextProvider>
-    </>
-  )
-}
+        <SessionContextProvider
+          supabaseClient={supabaseClient}
+          initialSession={pageProps.initialSession}
+        >
+          <Layout>
+            <Component {...pageProps} />
+          </Layout>
+        </SessionContextProvider>
+      </>
+    ))
 
-export default MyApp
+  return getLayout(<Component {...pageProps} />)
+}
--- publics/pages/index.tsx
+++ publics/pages/index.tsx
@@ -1,14 +1,42 @@
-import Auth from "../components/Auth"
+import Layout from "../components/LayoutMinimal"
+import LoginButton from "../components/LoginButton"
 import { createServerSupabaseClient } from "@supabase/auth-helpers-nextjs"
 import { Session } from "@supabase/auth-helpers-react"
 import type { AppProps } from "next/app"
+import Link from "next/link"
 
-export default function Home(pageProps: AppProps<{ initialSession: Session }>) {
+export default function Index(
+  pageProps: AppProps<{ initialSession: Session }>
+) {
   return (
-    <div>
-      <Auth />
+    <div
+      className="hero min-h-screen"
+      style={{ backgroundImage: `url("/nod.jpg")` }}
+    >
+      <div className="hero-overlay bg-black opacity-60"></div>
+      <div className="hero-content text-center">
+        <div className="font-bold text-white text-lg">
+          <div className="absolute top-10 left-10">Party Owl</div>
+          <div className="absolute top-10 right-36 hover:underline">
+            <Link href="/about">About</Link>
+          </div>
+          <div className="absolute top-10 right-10 hover:underline">
+            <Link href="mailto:awj3@rice.edu">Feedback</Link>
+          </div>
+        </div>
+        <div>
+          <h1 className="text-4xl font-bold mb-8 text-white">
+            Streamlining publics parties at Rice.
+          </h1>
+          <LoginButton />
+        </div>
+      </div>
     </div>
   )
+}
+
+Index.getLayout = function getLayout(page) {
+  return <Layout>{page}</Layout>
 }
 
 export const getServerSideProps = async (ctx) => {
