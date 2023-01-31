24223011d0e8c5d5985782b20596b4b3d34f14e1
spoken-angus <angusjelinek@gmail.com>
2023-01-31 14:48:18 -0600

    working fix for homepage

--- publics/components/Auth.tsx
+++ publics/components/Auth.tsx
@@ -1,29 +1,34 @@
+import { handleLogin } from "../utils/login"
 import LoginButton from "./LoginButton"
+import { useSupabaseClient } from "@supabase/auth-helpers-react"
+import Link from "next/link"
 
 export default function Auth() {
+  const supabaseClient = useSupabaseClient()
   return (
-    <div className="hero min-h-screen bg-base-200">
-      <div className="hero-content flex-col lg:flex-row-reverse">
-        <img
-          src="/martelpic.jpg"
-          className="ml-2 max-w-sm rounded-lg shadow-2xl"
-        />
-        <div className="lg:max-w-2/5 md:max-w-2/3">
-          <h1 className="text-4xl font-bold">
-            Streamlining public parties at Rice.
+    <div
+      className="hero min-h-screen"
+      style={{
+        backgroundImage: `-webkit-image-set(
+        url("nod.avif") type("image/avif"),
+        url("nod.jpg") type("image/jpeg"))`,
+      }}
+    >
+      <div className="hero-overlay bg-black opacity-60"></div>
+      <div className="hero-content text-center">
+        <div className="font-bold text-white text-lg">
+          <div className="absolute top-10 left-10">Party Owl</div>
+          <div className="absolute top-10 right-36 hover:underline">
+            <Link href="/about">About</Link>
+          </div>
+          <div className="absolute top-10 right-10 hover:underline">
+            <Link href="mailto:awj3@rice.edu">Feedback</Link>
+          </div>
+        </div>
+        <div>
+          <h1 className="text-4xl font-bold mb-8 text-white">
+            Streamlining publics parties at Rice.
           </h1>
-          <p className="py-6 text-xl">
-            <span className="text-primary">Attendees</span> can register for
-            events in-app and access important event information.
-          </p>
-          <p className="py-6 text-xl">
-            <span className="text-primary">Volunteers</span> can seamlessly
-            check in and out of their shift and track event capacity.
-          </p>
-          <p className="py-6 text-xl">
-            <span className="text-primary">Socials</span> can create events,
-            manage attendees and volunteers, and view event analytics.
-          </p>
           <LoginButton />
         </div>
       </div>
