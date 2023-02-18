c85cdae159c707af8b9a8b019f273e401ae469ad
nram1 <90943799+nram1@users.noreply.github.com>
2023-02-18 15:09:46 -0600

    working on delete event for event card

---
+++ publics/components/DeleteMsg.tsx
@@ -0,0 +1,18 @@
+export default function Delete() {
+  return (
+    <div className="modal">
+      <div className="modal-box">
+        <h3 className="font-bold text-lg">
+          Congratulations random Internet user!
+        </h3>
+        <p className="py-4">
+          You've been selected for a chance to get one year of subscription to
+          use Wikipedia for free!
+        </p>
+        <div className="modal-action">
+          <label className="btn">Yay!</label>
+        </div>
+      </div>
+    </div>
+  )
+}
