26bccb592ba8cc740386d0893705231159ce6544
Evan-Stegall <evanstegall007@gmail.com>
2023-02-18 14:26:37 -0600

    updated app to be same as main

--- publics/pages/_app.tsx
+++ publics/pages/_app.tsx
@@ -26,38 +26,7 @@ export default function MyApp({
     Component.getLayout ||
     ((page) => (
       <>
-<<<<<<< HEAD
-        <Head>
-          <link rel="icon" href="/owl.png" />
-          <meta
-            name="google-site-verification"
-            content="pQJj2FGXHM4uMmvH23WE8XS_7GPJIiCUXMwqc_24wOI"
-          />
-        </Head>
-        <DefaultSeo
-          title="PartyOwl"
-          description="PartyOwl streamlines public parties at Rice University with useful features for attendees, volunteers, and socials."
-          openGraph={{
-            type: "website",
-            locale: "en_uS",
-            url: "https://publics.rice.edu/",
-            siteName: "PartyOwl",
-            images: [
-              {
-                url: "https://publics.vercel.app/owl.png",
-                width: 712,
-                height: 512,
-                alt: "Owl",
-              },
-            ],
-          }}
-          twitter={{
-            cardType: "summary_large_image",
-          }}
-        />
-=======
         <Seo />
->>>>>>> fdfa501 (seo fixed)
         <SessionContextProvider
           supabaseClient={supabaseClient}
           initialSession={pageProps.initialSession}
