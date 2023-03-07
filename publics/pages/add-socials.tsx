54b2c5dfb9233675d6f3a85eaaefff7fc12cf332
Evan-Stegall <evanstegall007@gmail.com>
2023-03-07 13:24:10 -0600

    fixed select having Hanszen as default value. Added some styling and a couple null checks

--- publics/pages/add-socials.tsx
+++ publics/pages/add-socials.tsx
@@ -5,9 +5,6 @@ import {
   } from "@supabase/auth-helpers-nextjs"
 import { useSupabaseClient } from "@supabase/auth-helpers-react"
 import {useState} from "react";
-import { profile } from "console";
-import { env } from "process";
-
 /**
  * Simple type containing info about a college
  */
@@ -16,6 +13,9 @@ type CollegeDetails = {
     name: string
 }
 
+/*
+* Simple backend call that gets a list of all the colleges and their UUID's
+*/
 async function getColleges(supabase): Promise<CollegeDetails[]> {
     let colleges: CollegeDetails[] = [];
 
@@ -62,6 +62,7 @@ export const getServerSideProps = async (ctx) => {
         }
     }
 
+    /* Checks if the current user is a super admin, if they aren't then boot them from the page */
     const is_superadmin = await supabase.from("superadmins").select("*").eq("profile", session.user.id).single();
     
     if(!is_superadmin.data || is_superadmin.data.length < 1) {
@@ -84,12 +85,21 @@ export const getServerSideProps = async (ctx) => {
 
   function addSocials(props) {
     const supabase = useSupabaseClient();
+    //college that the superadmin selects to add a social to 
     const [selectedEvent, setSelectedEvent] = useState<string>("None");
+    //netid of the person the superadmin is trying to make a social
     const [netID, setNetID] = useState<string>("None");
+    //UUID of the supabase entry associated with netid
     const [supabaseProfileID, setSupabaseProfileID] = useState<string>("");
+    //Text displayed on modal that pops up when a superadmin tries to add a social
     const [modalText, setModalText] = useState<string>("Loading...")
+    //Text that gets below normal inputs. This indicates the success/failure of whatever you just tried to do
     const [actionStatusText, setActionStatusText] = useState<string>("");
 
+    /*
+    * Handles input from user and populates modal with appropriate text. 
+    * I.e. if the superadmin puts in a bad netID this will indicate as such on the modal that pops up 
+    */
     async function handleAddSocial(props, supabase, selectedEvent: string, netID: string, setModalText, setSupabaseProfileID) {
         const profile_data = await supabase
         .from("profiles")
@@ -112,9 +122,12 @@ export const getServerSideProps = async (ctx) => {
 
         let full_name = profile_data.data.first_name + " " + profile_data.data.last_name;
 
-        setModalText("Are you sure you want to set " + full_name + " as an admin for " + event_name[0].name + "?");
+        setModalText("Are you sure you want to add " + full_name + " as an admin for " + event_name[0].name + "?");
     }
 
+    /*
+    * Populates backend with new social that the superadmin selected
+    */
     async function addSocial(supabase, supabaseProfileID: string, selectedEvent: string, setActionStatusText) {
         /* Checks if this person is already an admin for this event */
         const is_already_there = await supabase
@@ -122,6 +135,11 @@ export const getServerSideProps = async (ctx) => {
         .select("id")
         .eq("profile", supabaseProfileID)
         .eq("organization", selectedEvent);
+
+        if(is_already_there.error) {
+            setActionStatusText("An error occured. Check your inputs and please try again!");
+            return;
+        }
 
         if (is_already_there.data.length > 0) {            
             setActionStatusText("That person is already an admin for the selected college!");
@@ -144,7 +162,7 @@ export const getServerSideProps = async (ctx) => {
         <div>
             <div className="flex max-w-full items-center justify-center">
                 <input type="text" placeholder="Enter netID here" className="input input-bordered w-25 mr-3" onChange = {(e) => setNetID(e.target.value)} />
-                <select className="select select-primary w-full max-w-xs mr-3" value = {selectedEvent} onChange = {(e) => setSelectedEvent(e.target.value)} >
+                <select className="select select-primary w-full max-w-xs mr-3" onChange = {(e) => setSelectedEvent(e.target.value)} >
                     <option disabled selected>Select Event Here</option>
                     {props.colleges.map(datum => {
                         return <option value = {datum.id}>{datum.name}</option>
@@ -157,11 +175,13 @@ export const getServerSideProps = async (ctx) => {
             <div className="modal">
                 <div className="modal-box">
                     <div><h4>{modalText}</h4></div>
-                    <label htmlFor="add-social-modal" className="btn mr-3" onClick = {() => {addSocial(supabase, supabaseProfileID, selectedEvent, setActionStatusText); setModalText("Loading...")}}>Add Social</label>
-                    <label htmlFor="add-social-modal" className="btn" onClick = {() => {setModalText("Loading..."); setActionStatusText("")}}>Exit</label>
+                    <div className = "flex items-end justify-end mt-5">
+                        <label htmlFor="add-social-modal" className="btn mr-3" onClick = {() => {addSocial(supabase, supabaseProfileID, selectedEvent, setActionStatusText); setModalText("Loading...")}}>Add Social</label>
+                        <label htmlFor="add-social-modal" className="btn" onClick = {() => {setModalText("Loading..."); setActionStatusText("")}}>Cancel</label>
+                    </div>
                 </div>
             </div>
-            <div><h4>{actionStatusText}</h4></div>
+            <div className = "flex items-center justify-center mt-10"><h4>{actionStatusText}</h4></div>
         </div>
     );
   }
